66aa88ff04892d734969cb60e2fe20390b446e88
kEllie <53048761+kEllieGit@users.noreply.github.com>
2025-02-07 16:21:06 +0200

    Enabler uses lists
    So we can target multiple gameobjects/components at once.

--- Code/Anomalies/Enabler.cs
+++ Code/Anomalies/Enabler.cs
@@ -3,21 +3,58 @@
 public class Enabler : Anomaly
 {
 	[Property] public override AnomalyType Type { get; set; } = AnomalyType.Extra;
-	[Property] public Component? Component { get; set; }
+
+	[Property] public List<GameObject> GameObjects { get; set; } = [];
+	[Property] public List<Component> ComponentList { get; set; } = [];
 
 	public override void OnAnomalyActive()
 	{
-		if ( Component.IsValid() )
-			Component.Enabled = !Component.Enabled;
-		
+		Toggle();
 		base.OnAnomalyActive();
 	}
 
 	public override void OnAnomalyClear()
 	{
-		if ( Component.IsValid() )
-			Component.Enabled = !Component.Enabled;
-		
+		Toggle();
 		base.OnAnomalyClear();
+	}
+
+	public void Toggle()
+	{
+		foreach ( var gameObject in GameObjects.Where( x => x.IsValid() ) )
+		{
+			gameObject.Enabled = !gameObject.Enabled;
+		}
+
+		foreach ( var component in ComponentList.Where( x => x.IsValid() ) )
+		{
+			component.Enabled = !component.Enabled;
+		}
+	}
+
+	public void Enable()
+	{
+		foreach ( var gameObject in GameObjects.Where( x => x.IsValid() ) )
+		{
+			gameObject.Enabled = true;
+		}
+
+		foreach ( var component in ComponentList.Where( x => x.IsValid() ) )
+		{
+			component.Enabled = true;
+		}
+	}
+
+	public void Disable()
+	{
+		foreach ( var gameObject in GameObjects.Where( x => x.IsValid() ) )
+		{
+			gameObject.Enabled = false;
+		}
+
+		foreach ( var component in ComponentList.Where( x => x.IsValid() ) )
+		{
+			component.Enabled = false;
+		}
 	}
 }
