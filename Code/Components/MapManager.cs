09f66b2b6638d3ed7089e32ad905c1bd5afb6cd7
kEllie <53048761+kEllieGit@users.noreply.github.com>
2025-02-10 19:49:21 +0200

    Add highest rank & highest percentages achieved to map definition

--- Code/Components/MapManager.cs
+++ Code/Components/MapManager.cs
@@ -1,9 +1,10 @@
-﻿namespace Observation;
+﻿using System;
+namespace Observation;
 
 public class MapManager : Component
 {
 	public static MapManager? Instance { get; private set; }
-	
+
 	[Property, InlineEditor, ReadOnly] public Map? ActiveMap { get; private set; }
 
 	protected override void OnStart()
@@ -25,11 +26,11 @@ public class MapManager : Component
 		{
 			return;
 		}
-		
+
 		var scene = ActiveMap.Scene;
 		if ( scene.IsValid() )
 			Scene.Load( scene );
-		
+
 		Sandbox.Services.Stats.Increment( "Restarts", 1 );
 		Sandbox.Services.Stats.Increment( $"Restarts_on_map_{ActiveMap.Ident}", 1 );
 	}
@@ -44,4 +45,38 @@ public class Map
 	public SceneFile? Scene { get; set; }
 	public Observation.Platform.Achievement? WinAchievement { get; set; }
 	[Title( "S Rank Achievement" )] public Observation.Platform.Achievement? SRankAchievement { get; set; }
+
+	public Rank? HighestRankAchieved => GetHighestRankAchieved() ?? Rank.F;
+	public double? HighestPercentageAchieved => GetHighestPercentageAchieved() ?? 0.0;
+
+	private const string GameIdent = "spoonstuff.observation";
+
+	private Rank? GetHighestRankAchieved()
+	{
+		var stats = Sandbox.Services.Stats.GetLocalPlayerStats( GameIdent );
+
+		foreach ( var rank in Enum.GetValues<Rank>().OrderBy( r => r ) )
+		{
+			var statName = $"Wins_on_map_{Ident}_with_rank_{rank}";
+			if ( stats.TryGet( statName, out var stat ) && stat.Value > 0 )
+			{
+				return rank;
+			}
+		}
+
+		return null;
+	}
+
+	private double? GetHighestPercentageAchieved()
+	{
+		var stats = Sandbox.Services.Stats.GetLocalPlayerStats( GameIdent );
+
+		var statName = $"Success_rate_on_map_{Ident}";
+		if ( stats.TryGet( statName, out var stat ) )
+		{
+			return stat.Max;
+		}
+
+		return null;
+	}
 }
