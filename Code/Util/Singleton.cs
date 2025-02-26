d88324fdf3f16678eeaef7f021ad3695d0e646f8
kEllie <53048761+kEllieGit@users.noreply.github.com>
2025-02-26 22:38:39 +0200

    Add singleton

---
+++ Code/Util/Singleton.cs
@@ -0,0 +1,42 @@
+﻿namespace Observation;
+
+public abstract class Singleton<T> : Component, IHotloadManaged where T : Singleton<T>
+{
+	public static T? Instance { get; private set; }
+
+	protected override void OnAwake()
+	{
+		if ( Instance.IsValid() )
+		{
+			Log.Warning( $"Singleton tried to initialize another {typeof(T)}!" );
+			Destroy();
+			return;
+		}
+
+		if ( Active )
+		{
+			Instance = (T)this;
+		}
+	}
+
+	void IHotloadManaged.Destroyed( Dictionary<string, object> state )
+	{
+		state["IsActive"] = Instance == this;
+	}
+
+	void IHotloadManaged.Created( IReadOnlyDictionary<string, object> state )
+	{
+		if ( state.GetValueOrDefault( "IsActive" ) is true )
+		{
+			Instance = (T)this;
+		}
+	}
+
+	protected override void OnDestroy()
+	{
+		if ( Instance == this )
+		{
+			Instance = null;
+		}
+	}
+}
