7aeecbfdfd96c118397e31de8c9e93311ab9888d
kEllieDev <kEllieDev@users.noreply.github.com>
2025-08-16 16:16:44 +0300

    Add missing translation for ReportingScreen

--- Code/UI/Gameplay/ReportingScreen.razor.cs
+++ Code/UI/Gameplay/ReportingScreen.razor.cs
@@ -4,13 +4,13 @@ namespace Observation.UI;
 
 public partial class ReportingScreen
 {
-	public Label? Report;
+	public Label? Report { get; private set; }
 
 	public void SetReport( bool validReport )
 	{
 		if ( Report.IsValid() )
 		{
-			Report.Text = validReport ? "Anomaly Cleared!" : "No Anomalies Found!";
+			Report.Text = validReport ? "#ui.report.clear" : "#ui.report.none";
 		}
 	}
 }
