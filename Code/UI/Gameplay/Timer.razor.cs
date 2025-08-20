f725937c77aec1e06dd8769d6a044522e85f8034
kEllieDev <kEllieDev@users.noreply.github.com>
2025-08-20 16:45:56 +0300

    Only show this border in Timer if camera has name

--- Code/UI/Gameplay/Timer.razor.cs
+++ Code/UI/Gameplay/Timer.razor.cs
@@ -6,9 +6,10 @@ public partial class Timer
 {
 	private static GameTimer? TimerInstance => GameTimer.Instance;
 	private static string ElapsedTime => TimerInstance?.GetDisplayTime() ?? "N/A";
+	private static bool ActiveCameraHasName => !string.IsNullOrEmpty( CameraManager.Instance?.ActiveCamera?.Name );
 
 	protected override int BuildHash()
 	{
-		return HashCode.Combine( TimerInstance?.SinceStart.Relative );
+		return HashCode.Combine( TimerInstance?.SinceStart.Relative, ActiveCameraHasName );
 	}
 }
